de3b4095084e34877c2e7dde0e569b0dea1345d5
Ryjedd <rlambert92@live.ca>
2017-01-01 23:34:01 -0500

    Tutorial 6
    Jump, custom values

--- Phaser-Minimal-Setup-master/js/main.js
+++ Phaser-Minimal-Setup-master/js/main.js
@@ -4,14 +4,23 @@ var cursors;
 function preload() {
 	
 	game.load.image('tux', 'res/tux.png');
+	game.load.image('brick', 'res/brick.png');
+
 }
 
 function create() {
 	game.stage.backgroundColor = '#55F';
 	game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+	//player
 	players = game.add.group();
 	players.enableBody = true;
-	createPlayer(10,10);
+	createPlayer(10,10,-300, 150);
+	
+	//floor
+	platforms = game.add.group();
+	platforms.enableBody = true;
+	createPlatform();
 	
 	cursors = game.input.keyboard.createCursorKeys();
 }
@@ -20,21 +29,37 @@ function update() {
 	playerUpdate();
 }
 
-function createPlayer(x,y){
+function createPlayer(x,y,j,v){
 	var player = players.create(x,y,'tux');
 	player.body.bounce.y = 0.2;
 	player.body.gravity.y = 300;
 	player.body.collideWorldBounds = true;
+	
+	player.jump = j;
+	player.v = v
+}
+
+function createPlatform(){
+	for(var i=0;i<game.world.width;i+=64){
+		var ground = platforms.create(i,game.world.height - 64, 'brick');
+		ground.body.immovable = true;
+	}
 }
 
 function playerUpdate(){
 	game.physics.arcade.collide(players, players);
+	game.physics.arcade.collide(players, platforms);
 	players.forEach(function(p){
 		p.body.velocity.x = 0;
 		if(cursors.left.isDown){
-			p.body.velocity.x = -150;
+			p.body.velocity.x = -p.v;
 		}else if(cursors.right.isDown){
-			p.body.velocity.x = 150;
+			p.body.velocity.x = p.v;
+		}
+		
+		//jump controls
+		if(cursors.up.isDown && p.body.touching.down){
+			p.body.velocity.y = p.jump;
 		}
 	});
 }
